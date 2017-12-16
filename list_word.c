6e7c40eae20c22ea7c59ffd5f2a12c7059f4a521
rizualam <ssrizwan25@gmail.com>
2017-12-16 22:22:49 +0530

    list_word

---
+++ list_word.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a[100];
+    int i=0,count=1;
+    printf("enter the sentence\n");
+    gets(a);
+    if(a[i]=='\0')
+    {
+    count=0;
+    }
+    else{
+        while (a[i]!='\0')
+        {
+            if (a[i]==' ')
+            {
+                count++;
+            }
+            i++;
+        }
+    }
+    printf("no.of words in sentence=%d",count);
+    return 0;
+}
+
